5a1a1f6268a52c46f0df8c742d6eb2e030244c5b
patrickjiang <patrick.jiang86@gmail.com>
2020-12-08 15:35:15 -0500

    finish app1

--- src/app1.js
+++ src/app1.js
@@ -1,53 +1,92 @@
 import "./app1.css"
 import $ from "jquery"
 
+const eventBus = $({})
+const m = {
+    data: {
+        n: parseInt(localStorage.getItem('n'))
+    },
+    create() {
+    },
+    delete() {
+    },
+    update(data) {
+        Object.assign(m.data, data)
+        eventBus.trigger('m:updated')
+    },
+    get() {
+    },
+
+}
 const v = {
+    el: null,
     html: `
-    <section id="app1">
-        <div class="output"><span id="number">100</span></div>
+    <div>
+        <div class="output"><span id="number">{{n}}</span></div>
         <div class="actions">
             <button id="add">+1</button>
             <button id="minus">-1</button>
             <button id="times">×2</button>
             <button id="divide">÷2</button>
         </div>
-    </section>
+    </div>
 `,
-    render() {
-        $(v.html).prependTo($('body > .page'));
+    init(el) {
+        v.el = $(el)
+
+    },
+    render(n) {
+        if (v.el.children.length !== 0) {
+            v.el.empty();
+        }
+        $(v.html.replace('{{n}}', n)).appendTo(v.el);
     }
 }
 
-v.render()
 
-const $button1 = $('#add')
-const $button2 = $('#minus ')
-const $button3 = $('#times')
-const $button4 = $('#divide')
-const $number = $('#number')
-const n = localStorage.getItem('n')
-$number.text(n || 100)
-$button1.on('click', () => {
-    let n = parseInt($number.text())
-    n += 1
-    localStorage.setItem('n', n)
-    $number.text(n)
-})
-$button2.on('click', () => {
-    let n = parseInt($number.text())
-    n -= 1
-    localStorage.setItem('n', n)
-    $number.text(n)
-})
-$button3.on('click', () => {
-    let n = parseInt($number.text())
-    n *= 2
-    localStorage.setItem('n', n)
-    $number.text(n)
-})
-$button4.on('click', () => {
-    let n = parseInt($number.text())
-    n /= 2
-    localStorage.setItem('n', n)
-    $number.text(n)
-})
+const c = {
+    init(container) {
+        v.init(container)
+        v.render(m.data.n)
+        c.autoBindEvents()
+        eventBus.on('m:updated', () => {
+            v.render(m.data.n)
+        })
+    },
+    events: {
+        'click #add': 'add',
+        'click #minus': 'minus',
+        'click #times': 'times',
+        'click #divide': 'divide',
+    },
+    add() {
+        m.update(m.data.n += 1)
+    },
+    minus() {
+        m.update(m.data.n -= 1)
+    },
+    times() {
+        m.update(m.data.n *= 2)
+    },
+    divide() {
+        m.update(m.data.n /= 2)
+    },
+    autoBindEvents() {
+        for (let key in c.events) {
+            const spaceIndex = key.indexOf(' ')
+            const value = c[c.events[key]]
+            const part1 = key.slice(0, spaceIndex)
+            const part2 = key.slice(spaceIndex + 1)
+            v.el.on(part1, part2, value)
+        }
+    }
+}
+
+
+export default c
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import "./reset.css"
 import "./styles.css"
 
-import "./app1.js"
+import c from "./app1.js"
 import "./app2.js"
 import "./app3.js"
 import "./app4.js"
+
+c.init('.app1')
